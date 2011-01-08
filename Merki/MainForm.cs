14a10e7c257c7e460637d9e4e9b64e4fb83ba0be
nado18 <nado18@gmail.com>
2011-01-07 20:20:08 -0500

    Changing the Interface namespace to Merki

--- Merki/MainForm.cs
+++ Merki/MainForm.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using Markup;
 
-namespace Interface
+namespace Merki
 {
     public partial class MainForm : Form
     {
--- Merki/Program.cs
+++ Merki/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace Interface
+namespace Merki
 {
     static class Program
     {
