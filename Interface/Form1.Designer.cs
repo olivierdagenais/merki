c8cb26f9bf94ea28ff583a844b4480a6a4657616
nado18 <nado18@gmail.com>
2011-01-07 19:45:10 -0500

    First version of the user interface

--- Interface/Form1.Designer.cs
+++ Interface/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Interface
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.searchText = new System.Windows.Forms.ToolStripTextBox();
+            this.searchResultsBox = new System.Windows.Forms.GroupBox();
+            this.searchResults = new System.Windows.Forms.ListView();
+            this.editor = new System.Windows.Forms.TextBox();
+            this.split = new System.Windows.Forms.SplitContainer();
+            this.editBox = new System.Windows.Forms.GroupBox();
+            this.previewBox = new System.Windows.Forms.GroupBox();
+            this.menu.SuspendLayout();
+            this.searchResultsBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.split)).BeginInit();
+            this.split.Panel1.SuspendLayout();
+            this.split.Panel2.SuspendLayout();
+            this.split.SuspendLayout();
+            this.editBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.searchText});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menu.Size = new System.Drawing.Size(448, 25);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // searchText
+            // 
+            this.searchText.Name = "searchText";
+            this.searchText.Size = new System.Drawing.Size(100, 21);
+            // 
+            // searchResultsBox
+            // 
+            this.searchResultsBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchResultsBox.Controls.Add(this.searchResults);
+            this.searchResultsBox.Location = new System.Drawing.Point(12, 29);
+            this.searchResultsBox.Name = "searchResultsBox";
+            this.searchResultsBox.Size = new System.Drawing.Size(424, 87);
+            this.searchResultsBox.TabIndex = 2;
+            this.searchResultsBox.TabStop = false;
+            this.searchResultsBox.Text = "Search Results";
+            // 
+            // searchResults
+            // 
+            this.searchResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchResults.FullRowSelect = true;
+            this.searchResults.Location = new System.Drawing.Point(7, 20);
+            this.searchResults.Name = "searchResults";
+            this.searchResults.Size = new System.Drawing.Size(411, 61);
+            this.searchResults.TabIndex = 0;
+            this.searchResults.UseCompatibleStateImageBehavior = false;
+            this.searchResults.View = System.Windows.Forms.View.List;
+            // 
+            // editor
+            // 
+            this.editor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.editor.Location = new System.Drawing.Point(0, 19);
+            this.editor.Multiline = true;
+            this.editor.Name = "editor";
+            this.editor.Size = new System.Drawing.Size(200, 145);
+            this.editor.TabIndex = 0;
+            // 
+            // split
+            // 
+            this.split.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.split.Location = new System.Drawing.Point(12, 123);
+            this.split.Name = "split";
+            // 
+            // split.Panel1
+            // 
+            this.split.Panel1.Controls.Add(this.editBox);
+            // 
+            // split.Panel2
+            // 
+            this.split.Panel2.Controls.Add(this.previewBox);
+            this.split.Size = new System.Drawing.Size(424, 171);
+            this.split.SplitterDistance = 212;
+            this.split.TabIndex = 3;
+            // 
+            // editBox
+            // 
+            this.editBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.editBox.Controls.Add(this.editor);
+            this.editBox.Location = new System.Drawing.Point(3, 4);
+            this.editBox.Name = "editBox";
+            this.editBox.Size = new System.Drawing.Size(206, 164);
+            this.editBox.TabIndex = 0;
+            this.editBox.TabStop = false;
+            this.editBox.Text = "Edit";
+            // 
+            // previewBox
+            // 
+            this.previewBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.previewBox.Location = new System.Drawing.Point(3, 4);
+            this.previewBox.Name = "previewBox";
+            this.previewBox.Size = new System.Drawing.Size(202, 164);
+            this.previewBox.TabIndex = 0;
+            this.previewBox.TabStop = false;
+            this.previewBox.Text = "Preview";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 306);
+            this.Controls.Add(this.split);
+            this.Controls.Add(this.searchResultsBox);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.searchResultsBox.ResumeLayout(false);
+            this.split.Panel1.ResumeLayout(false);
+            this.split.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.split)).EndInit();
+            this.split.ResumeLayout(false);
+            this.editBox.ResumeLayout(false);
+            this.editBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripTextBox searchText;
+        private System.Windows.Forms.GroupBox searchResultsBox;
+        private System.Windows.Forms.ListView searchResults;
+        private System.Windows.Forms.TextBox editor;
+        private System.Windows.Forms.SplitContainer split;
+        private System.Windows.Forms.GroupBox editBox;
+        private System.Windows.Forms.GroupBox previewBox;
     }
 }
 
